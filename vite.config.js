204e984757d4904b6233ca718e987c24d95186b7
szeedneigh <sidneyjohn1704@gmail.com>
2025-01-22 23:20:55 +0800

    Add fallback

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from "vite"
  
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
