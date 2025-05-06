0157e5c919f99f5811fea076a9c5f97865418bd0
szeedneigh <sidneyjohn1704@gmail.com>
2025-05-06 23:38:08 +0800

    chore: mod routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LandingPage from './app/landing/LandingPage';
+import LandingPage from './app/pages/LandingPage';
 import SignUp from './app/auth/SignUp';
 import LogIn from './app/auth/LogIn';
-import Dashboard from './pages/Dashboard';
-import NotFound from './pages/NotFound';
+import Dashboard from './app/pages/Dashboard';
+import NotFound from './app/pages/NotFound';
 import ForgotPassword from './app/auth/ForgotPassword';
 
 function App() {
