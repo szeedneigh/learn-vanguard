72ce7b648a22e5c3b7d9426aa8413792df798f74
szeedneigh <sidneyjohn1704@gmail.com>
2025-05-18 23:42:32 +0800

    chore: add provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './Globals.css'
 import App from './App.jsx'
+import { AuthProvider } from './context/AuthContext'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from 'react-hot-toast'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <App />
+        <Toaster
+          position="top-right"
+          reverseOrder={false}
+        />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
